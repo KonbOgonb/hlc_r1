d08fc54ec60d6783c0e3fac3286ee0d2deeb3bbd
Anton Gordeev <agordeev@OneIncSystems.com>
2017-08-30 18:09:56 +0700

    Some tuning on config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ echo Starting memcached.
 echo loading initial data
 python3 dataImporter.py
 echo Starting gunicorn.
-exec gunicorn app:app --bind 0.0.0.0:80 --workers 21
+exec gunicorn app:app --bind 0.0.0.0:80 --workers 11 --keep-alive 5 --preload True --timeout 30 --graceful-timeout 30 --backlog 4096
